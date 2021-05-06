fef09fb316500b9465c598df7da64c53abb67f6e
Vitor Borges de Oliveira <83785633+Vitor-Borges-De-Oliveira@users.noreply.github.com>
2021-05-06 18:50:33 -0300

    Create condicionais.py

---
+++ condicionais.py
@@ -0,0 +1,23 @@
+horario = 'noite'
+
+if horario == 'dia':
+   print('O sol está quente')
+elif horario == 'tarde':
+    print('O sol está chegando')
+else:
+    print('A lua está linda')
+    
+if nome == 'Felipe':                   
+    print('É o Felipe!')
+    
+if idade >= 23:
+    print('São tantos anos')
+   
+temperatura = 22
+    
+if temperatura <= 30:
+
+    print('Não está tão quente')
+
+else:
+    print('Está muito quente!')

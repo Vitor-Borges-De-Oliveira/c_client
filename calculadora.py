4ef030485bc5dee900b8a389c51bb67b52c7b9e0
Vitor Borges de Oliveira <83785633+Vitor-Borges-De-Oliveira@users.noreply.github.com>
2021-05-06 18:46:07 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+print('Bem-vindo a nossa Calculadora de multiplicação')
+
+numero1 = float(input('Digite o primeiro número '))
+numero2 = float2(input('Digite o segundo número '))
+
+resultado = numero1 * numero2
+
+print('O resultado é', resultado)
+
+Bem-vindo a nossa Calculadora de multiplicação
+Digite o primeiro número 10
+Digite o segundo número 20.5
+O resultado é 205.0
+
